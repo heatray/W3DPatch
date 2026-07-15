fc960a68b1179bc879c0f90772d81a99e1a8997a
heatray <heatrayws@gmail.com>
2026-07-15 22:57:21 +0300

    Refactor Patch

--- source/patch.cpp
+++ source/patch.cpp
@@ -2,16 +2,8 @@
 #include <mmsystem.h>
 #pragma comment(lib, "winmm.lib")
 
-float AspectRatio43 = 4.0f / 3.0f;
-float AspectRatio34 = 3.0f / 4.0f;
-float F2 = 2.0f;
-struct Screen
-{
-    int Width = 800;
-    int Height = 600;
-    float AspectRatioX = 4.0f / 3.0f;
-    float AspectRatioY = 3.0f / 4.0f;
-} Screen;
+float AspectRatioX = 4.0f / 3.0f;
+float AspectRatioY = 3.0f / 4.0f;
 struct Frustum
 {
     float Left = -0.64f;
@@ -19,173 +11,123 @@ struct Frustum
     float Bottom = -0.48f;
     float Top = 0.48f;
 } Frustum;
-struct MovieRect
+
+struct Worms3dApp
 {
-    int vX = 0;
-    int vY = 0;
-    int vW = 640;
-    int vH = 480;
-} MovieRect;
-bool AspectRatioFix, SleepFix;
+    char pad[0x20];
+    int RenderWidth;
+    int RenderHeight;
+};
+tagRECT MovieRect;
+bool AspectRatioFix, SleepFix, Borderless, NoMusic, NoMovies, NoMoviesIntro;
 uint8_t FrameInterval;
-
-bool NoMusic, NoMovies, NoMoviesIntro;
-
-bool Borderless;
-
 std::string GameSpyHost;
 struct GameSpy
 {
-    char ServerBrowser[128] = "%s.ms%d.";  // Master - Server Browser
-    char QueryReport[128] = "%s.master.";  // Master - Query Report 2
-    char ConnectionManager[128] = "gpcm."; // Presence - Connection Manager
-    char SearchPlayer[128] = "gpsp.";      // Presence - Search Player
-    char PeerChat[128] = "peerchat.";      // Chat
-    char GameStats[128] = "gamestats.";    // Game Statistics
-    char SdkDev[128] = "sdkdev.";          // SDK Dev
-} GS;
-
-float FE_Textbox_Back_Scale = 900.0f;
-float EFMV_Border_Width = 900.0f;
-
-DWORD* Worms3dApp__c_pTheInstance = (DWORD*)0x7ADDE4;
-DWORD AspectRatioCodeCaveExit = 0x629F72;
-DWORD FrustumCodeCaveExit = 0x44ADBB;
-DWORD SetFromSceneCameraCodeCaveExit = 0x63B469;
-DWORD MoviePlayerPCCodeCaveExit = 0x61A06F;
+    std::string ServerBrowser = "%s.ms%d.";  // Master - Server Browser
+    std::string QueryReport = "%s.master.";  // Master - Query Report 2
+    std::string ConnectionManager = "gpcm."; // Presence - Connection Manager
+    std::string SearchPlayer = "gpsp.";      // Presence - Search Player
+    std::string PeerChat = "peerchat.";      // Chat
+    std::string GameStats = "gamestats.";    // Game Statistics
+    std::string SdkDev = "sdkdev.";          // SDK Dev
+};
 
 void __declspec(naked) AspectRatioCodeCave()
 {
-    __asm {
-        mov     ecx, dword ptr ds : [0x7ADDE4]
-        mov     ecx, [ecx + 0x20]
-        mov     Screen.Width, ecx
-        mov     ecx, dword ptr ds : [0x7ADDE4]
-        mov     ecx, [ecx + 0x24]
-        mov     Screen.Height, ecx
-        fild    dword ptr ds : [Screen.Height]
-        fild    dword ptr ds : [Screen.Width]
-        fdivp   st(1), st
-        fld     dword ptr ds : [Screen.AspectRatioY]
-        fcomip  st, st(1)
-        fstp    st
-        jne     AspectRatioX
-        jmp     AspectRatioCodeCaveExit
+    static const DWORD Exit = 0x629F72;
+    static Worms3dApp* App;
+    static int Width, Height;
 
-    AspectRatioX :
-        jb      AspectRatioY
-        fild    dword ptr ds : [Screen.Width]
-        fild    dword ptr ds : [Screen.Height]
-        fdivp   st(1), st
-        fstp    dword ptr ds : [Screen.AspectRatioX]
-        fld     dword ptr ds : [Screen.AspectRatioX]
-        fmul    dword ptr ds : [Frustum.Bottom]
-        fstp    dword ptr ds : [Frustum.Left]
-        fld     dword ptr ds : [Screen.AspectRatioX]
-        fmul    dword ptr ds : [Frustum.Top]
-        fstp    dword ptr ds : [Frustum.Right]
-        jmp     AspectRatioCodeCaveExit
+    __asm pushad
 
-    AspectRatioY :
-        fild    dword ptr ds : [Screen.Height]
-        fild    dword ptr ds : [Screen.Width]
-        fdivp   st(1), st
-        fstp    dword ptr ds : [Screen.AspectRatioY]
-        fld     dword ptr ds : [Screen.AspectRatioY]
-        fmul    dword ptr ds : [Frustum.Left]
-        fstp    dword ptr ds : [Frustum.Bottom]
-        fld     dword ptr ds : [Screen.AspectRatioY]
-        fmul    dword ptr ds : [Frustum.Right]
-        fstp    dword ptr ds : [Frustum.Top]
-        jmp     AspectRatioCodeCaveExit
+    App = *reinterpret_cast<Worms3dApp**>(0x7ADDE4);
+    Width = App->RenderWidth;
+    Height = App->RenderHeight;
+    AspectRatioX = (float)Width / (float)Height;
+    AspectRatioY = (float)Height / (float)Width;
+    MovieRect.left = 0;
+    MovieRect.top = 0;
+    MovieRect.right = Width;
+    MovieRect.bottom = Height;
+
+    if (AspectRatioY < 3.0f / 4.0f)      // Hor+
+    {
+        Frustum.Left = AspectRatioX * Frustum.Bottom;
+        Frustum.Right = AspectRatioX * Frustum.Top;
+        MovieRect.right = (int)(MovieRect.bottom * 4.0f / 3.0f);
+        MovieRect.left = (int)((Width - MovieRect.right) / 2.0f);
     }
+    else if (AspectRatioY > 3.0f / 4.0f) // Vert+
+    {
+        Frustum.Bottom = AspectRatioY * Frustum.Left;
+        Frustum.Top = AspectRatioY * Frustum.Right;
+        MovieRect.bottom = (int)(MovieRect.right * 3.0f / 4.0f);
+        MovieRect.top = (int)((Height - MovieRect.bottom) / 2.0f);
+    }
+
+    __asm popad
+    __asm jmp Exit
 }
 
 void __declspec(naked) FrustumCodeCave()
 {
+    static const DWORD Exit = 0x44ADBB;
     __asm {
-        fld     dword ptr ds : [Frustum.Left]
-        fstp    dword ptr ds : [esp + 0x30]
-        fld     dword ptr ds : [Frustum.Right]
-        fstp    dword ptr ds : [esp + 0x34]
-        fld     dword ptr ds : [Frustum.Bottom]
-        fstp    dword ptr ds : [esp + 0x38]
-        fld     dword ptr ds : [Frustum.Top]
-        fstp    dword ptr ds : [esp + 0x3C]
-        jmp     FrustumCodeCaveExit
+        fld dword ptr ds : [Frustum.Left]
+        fstp dword ptr ds : [esp + 0x30]
+        fld dword ptr ds : [Frustum.Right]
+        fstp dword ptr ds : [esp + 0x34]
+        fld dword ptr ds : [Frustum.Bottom]
+        fstp dword ptr ds : [esp + 0x38]
+        fld dword ptr ds : [Frustum.Top]
+        fstp dword ptr ds : [esp + 0x3C]
+        jmp Exit
     }
 }
 
 void __declspec(naked) SetFromSceneCameraCodeCave()
 {
+    static const DWORD Exit = 0x63B45A;
+    static float ApertureX, ApertureY;
+
     __asm {
-        fld     dword ptr ds : [eax + 0x30]
-        fmul    dword ptr ds : [Screen.AspectRatioX]
-        fmul    st, st(1)
-        fstp    dword ptr ds : [ebp + 0xC]
-        fld     dword ptr ds : [eax + 0x2C]
-        fmul    dword ptr ds : [Screen.AspectRatioY]
-        push    edi
-        jmp     SetFromSceneCameraCodeCaveExit
+        fld dword ptr ds : [eax + 0x2C]
+        fstp ApertureX
+        fld dword ptr ds : [eax + 0x30]
+        fstp ApertureY
+        pushad
+    }
+
+    if (AspectRatioY < 3.0f / 4.0f)      // Hor+
+        ApertureX = ApertureY * AspectRatioX;
+    else if (AspectRatioY > 3.0f / 4.0f) // Vert+
+        ApertureY = ApertureX * AspectRatioY;
+
+    __asm {
+        popad
+        fld ApertureX
+        fstp dword ptr ds : [eax + 0x2C]
+        fld ApertureY
+        fstp dword ptr ds : [eax + 0x30]
+        fld dword ptr ds : [0x74A48C]
+        jmp Exit
     }
 }
 
 void __declspec(naked) MoviePlayerPCCodeCave()
 {
+    static const DWORD Exit = 0x61A06F;
     __asm {
-        mov     edx, dword ptr ss : [esp + 0x18] // Right
-        mov     dword ptr ds : [MovieRect.vW] , edx
-        mov     edx, dword ptr ss : [esp + 0x1C] // Bottom
-        mov     dword ptr ds : [MovieRect.vH] , edx
-        fild    dword ptr ds : [MovieRect.vH]
-        fidiv   dword ptr ds : [MovieRect.vW]
-        fld     dword ptr ds : [AspectRatio34]
-        fcomip  st(0), st(1)
-        fstp    st(0)
-        jne     AspectRatioX
-        jmp     MoviePlayerEnd
-
-    AspectRatioX :
-        // vW = B * (4 / 3)     vH = B
-        // vX = (R - vW) / 2    vY = 0
-        jb      AspectRatioY
-        fild    dword ptr ss : [esp + 0x18] // Width
-        fild    dword ptr ss : [esp + 0x1C] // Height
-        fld     dword ptr ds : [AspectRatio43]
-        fmul    st(0), st(1)
-        fistp   dword ptr ds : [MovieRect.vW]
-        fistp   dword ptr ds : [MovieRect.vH]
-        fisub   dword ptr ds : [MovieRect.vW]
-        fld     dword ptr ds : [F2]
-        fdivp   st(1), st(0)
-        fistp   dword ptr ds : [MovieRect.vX]
-        jmp     MoviePlayerEnd
-
-    AspectRatioY :
-        // vW = R    vH = R * (3 / 4)
-        // vX = 0    vY = (B - vH) / 2
-        fild    dword ptr ss : [esp + 0x1C] // Height
-        fild    dword ptr ss : [esp + 0x18] // Width
-        fld     dword ptr ds : [AspectRatio34]
-        fmul    st(0), st(1)
-        fistp   dword ptr ds : [MovieRect.vH]
-        fistp   dword ptr ds : [MovieRect.vW]
-        fisub   dword ptr ds : [MovieRect.vH]
-        fld     dword ptr ds : [F2]
-        fdivp   st(1), st(0)
-        fistp   dword ptr ds : [MovieRect.vY]
-        jmp     MoviePlayerEnd
-
-    MoviePlayerEnd :
-        mov     edx, dword ptr ds : [MovieRect.vH]
-        push    edx
-        mov     edx, dword ptr ds : [MovieRect.vW]
-        push    edx
-        mov     edx, dword ptr ds : [MovieRect.vY]
-        push    edx
-        mov     edx, dword ptr ds : [MovieRect.vX]
-        push    edx
-        jmp     MoviePlayerPCCodeCaveExit
+        mov edx, dword ptr ds : [MovieRect.bottom]
+        push edx
+        mov edx, dword ptr ds : [MovieRect.right]
+        push edx
+        mov edx, dword ptr ds : [MovieRect.top]
+        push edx
+        mov edx, dword ptr ds : [MovieRect.left]
+        push edx
+        jmp Exit
     }
 }
 
@@ -196,11 +138,15 @@ void Init()
     AspectRatioFix = iniReader.ReadInteger("Main", "AspectRatioFix", 0) == 1;
     SleepFix = iniReader.ReadInteger("Main", "SleepFix", 0) == 1;
     FrameInterval = iniReader.ReadInteger("Main", "FrameInterval", 16);
+
+    Borderless = iniReader.ReadInteger("Options", "Borderless", 0) == 1;
     NoMusic = iniReader.ReadInteger("Options", "NoMusic", 0) == 1;
     NoMovies = iniReader.ReadInteger("Options", "NoMovies", 0) == 1;
     NoMoviesIntro = iniReader.ReadInteger("Options", "NoMoviesIntro", 0) == 1;
-    Borderless = iniReader.ReadInteger("Window", "Borderless", 0) == 1;
+
     GameSpyHost = iniReader.ReadString("GameSpy", "Host", "gamespy.com");
+
+    // Main
 
     if (AspectRatioFix)
     {
@@ -208,21 +154,21 @@ void Init()
         injector::MakeJMP(0x629F6D, AspectRatioCodeCave);
 
         // Worms3dApp::InitScene
-        injector::WriteMemory<FLOAT*>(0x44AE0B, &Frustum.Right, true);
-        injector::WriteMemory<FLOAT*>(0x44AE2B, &Frustum.Top, true);
-
-        // XomHelp::Xom3dAppBase::CreateCamera
         injector::MakeJMP(0x44AD9B, FrustumCodeCave);
+        injector::WriteMemory<float*>(0x44AE0B, &Frustum.Right, true);
+        injector::WriteMemory<float*>(0x44AE2B, &Frustum.Top, true);
 
         // XCamera::SetFromSceneCamera
-        injector::MakeJMP(0x63B45D, SetFromSceneCameraCodeCave);
+        injector::MakeJMP(0x63B454, SetFromSceneCameraCodeCave);
 
         // PopUpEntity::Initialize
-        injector::WriteMemory<FLOAT>(0x5D648C, FE_Textbox_Back_Scale, true);
-        injector::WriteMemory<FLOAT>(0x5D6494, FE_Textbox_Back_Scale, true);
+        static float FE_Textbox_Back_Scale = 900.0f;
+        injector::WriteMemory<float>(0x5D648C, FE_Textbox_Back_Scale, true);
+        injector::WriteMemory<float>(0x5D6494, FE_Textbox_Back_Scale, true);
 
         // EfmvBorderEntity::EfmvBorderEntity
-        injector::WriteMemory<FLOAT*>(0x4EC2B6, &EFMV_Border_Width, true);
+        static float EFMV_Border_Width = 900.0f;
+        injector::WriteMemory<float*>(0x4EC2B6, &EFMV_Border_Width, true);
 
         // MoviePlayerPC::Open
         injector::MakeJMP(0x61A05F, MoviePlayerPCCodeCave);
@@ -240,10 +186,21 @@ void Init()
         injector::WriteMemory<BYTE>(0x44B541, FrameInterval, true);
     }
 
+    // Options
+
+    if (Borderless)
+    {
+        injector::WriteMemory<DWORD>(0x6C3F5B, 0x96030000, true);
+        injector::WriteMemory<DWORD>(0x6C3F65, 0x96030000, true);
+        injector::WriteMemory<BYTE>(0x69C6B2, 0, true);
+        injector::WriteMemory<BYTE>(0x69C6B4, 0, true);
+    }
+
     if (NoMusic)
     {
-        injector::WriteMemory<BYTE>(0x76FC60, 0x0, true);
+        injector::WriteMemory<BYTE>(0x76FC60, 0, true);
     }
+
     if (NoMovies || NoMoviesIntro)
     {
         BYTE OptionsByte1 = 0x93;
@@ -262,31 +219,26 @@ void Init()
         injector::WriteMemory<BYTE>(0x44A323, OptionsByte2, true);
     }
 
-    if (Borderless)
-    {
-        injector::WriteMemory<DWORD>(0x6C3F5B, 0x96030000, true);
-        injector::WriteMemory<DWORD>(0x6C3F65, 0x96030000, true);
-        injector::WriteMemory<BYTE>(0x69C6B2, 0, true);
-        injector::WriteMemory<BYTE>(0x69C6B4, 0, true);
-    }
+    // GameSpy
 
     if (GameSpyHost != "gamespy.com")
     {
-        strcat_s(GS.ServerBrowser, GameSpyHost.c_str());
-        strcat_s(GS.QueryReport, GameSpyHost.c_str());
-        strcat_s(GS.ConnectionManager, GameSpyHost.c_str());
-        strcat_s(GS.SearchPlayer, GameSpyHost.c_str());
-        strcat_s(GS.PeerChat, GameSpyHost.c_str());
-        strcat_s(GS.GameStats, GameSpyHost.c_str());
-        strcat_s(GS.SdkDev, GameSpyHost.c_str());
-        injector::WriteMemory(0x6112B7, &GS.ServerBrowser, true);
-        injector::WriteMemory(0x5F9E1A, &GS.QueryReport, true);
-        injector::WriteMemory(0x602FBA, &GS.ConnectionManager, true);
-        injector::WriteMemory(0x603C4D, &GS.SearchPlayer, true);
-        injector::WriteMemory(0x60B6F9, &GS.PeerChat, true);
-        injector::WriteMemory(0x5E8A52, &GS.GameStats, true);
-        injector::WriteMemory(0x5F4B1E, &GS.GameStats, true);
-        injector::WriteMemory(0x5E8A4D, &GS.SdkDev, true);
+        static GameSpy GS;
+        GS.ServerBrowser += GameSpyHost;
+        GS.QueryReport += GameSpyHost;
+        GS.ConnectionManager += GameSpyHost;
+        GS.SearchPlayer += GameSpyHost;
+        GS.PeerChat += GameSpyHost;
+        GS.GameStats += GameSpyHost;
+        GS.SdkDev += GameSpyHost;
+        injector::WriteMemory(0x6112B7, GS.ServerBrowser.c_str(), true);
+        injector::WriteMemory(0x5F9E1A, GS.QueryReport.c_str(), true);
+        injector::WriteMemory(0x602FBA, GS.ConnectionManager.c_str(), true);
+        injector::WriteMemory(0x603C4D, GS.SearchPlayer.c_str(), true);
+        injector::WriteMemory(0x60B6F9, GS.PeerChat.c_str(), true);
+        injector::WriteMemory(0x5E8A52, GS.GameStats.c_str(), true);
+        injector::WriteMemory(0x5F4B1E, GS.GameStats.c_str(), true);
+        injector::WriteMemory(0x5E8A4D, GS.SdkDev.c_str(), true);
     }
 }
 
