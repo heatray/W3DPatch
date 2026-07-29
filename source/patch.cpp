f7bb7282d8ddd9e548cc06a58ab299176760de2b
heatray <heatrayws@gmail.com>
2026-07-29 15:42:28 +0300

    Add Options

--- source/patch.cpp
+++ source/patch.cpp
@@ -2,6 +2,25 @@
 #include <mmsystem.h>
 #pragma comment(lib, "winmm.lib")
 
+bool AspectRatioFix, SleepFix, Borderless, NoMusic, NoVoices;
+char Fullscreen, NoAutoLoop, Options[40];
+uint8_t FrameInterval;
+std::string GameSpyHost;
+
+struct Worms3dApp
+{
+    char pad_0[0x20];
+    int RenderWidth;
+    int RenderHeight;
+    char Fullscreen;
+    char pad_29[0x9F];
+    char Options[8];
+};
+struct FlowControlService
+{
+    char pad_0[0xF4];
+    char AutoLoop;
+};
 float AspectRatioX = 4.0f / 3.0f;
 float AspectRatioY = 3.0f / 4.0f;
 struct Frustum
@@ -11,17 +30,7 @@ struct Frustum
     float Bottom = -0.48f;
     float Top = 0.48f;
 } Frustum;
-
-struct Worms3dApp
-{
-    char pad[0x20];
-    int RenderWidth;
-    int RenderHeight;
-};
 tagRECT MovieRect;
-bool AspectRatioFix, SleepFix, Borderless, NoMusic, NoMovies, NoMoviesIntro;
-uint8_t FrameInterval;
-std::string GameSpyHost;
 struct GameSpy
 {
     std::string ServerBrowser = "%s.ms%d.";  // Master - Server Browser
@@ -131,18 +140,165 @@ void __declspec(naked) MoviePlayerPCCodeCave()
     }
 }
 
+void __declspec(naked) OptionsCodeCave()
+{
+    static const DWORD Exit = 0x44D957;
+    static const DWORD Worms3dApp__ParsePreInitFinalCommands = 0x44C7B0;
+    static Worms3dApp* App;
+    // static FlowControlService* FCS;
+
+    __asm call Worms3dApp__ParsePreInitFinalCommands
+    __asm pushad
+
+    App = *reinterpret_cast<Worms3dApp**>(0x7ADDE4);
+
+    if      (Fullscreen == 1) App->Fullscreen |= 1;
+    else if (Fullscreen == 0) App->Fullscreen &= ~1;
+
+    if      (Options[0] == 1) App->Options[0] |= 0x01;
+    else if (Options[0] == 0) App->Options[0] &= ~0x01;
+    if      (Options[1] == 1) App->Options[0] |= 0x02;
+    else if (Options[1] == 0) App->Options[0] &= ~0x02;
+    if      (Options[2] == 1) App->Options[0] |= 0x04;
+    else if (Options[2] == 0) App->Options[0] &= ~0x04;
+    if      (Options[3] == 1) App->Options[0] |= 0x08;
+    else if (Options[3] == 0) App->Options[0] &= ~0x08;
+    if      (Options[4] == 1) App->Options[0] |= 0x10;
+    else if (Options[4] == 0) App->Options[0] &= ~0x10;
+    if      (Options[5] == 1) App->Options[0] |= 0x20;
+    else if (Options[5] == 0) App->Options[0] &= ~0x20;
+    if      (Options[6] == 1) App->Options[0] |= 0x40;
+    else if (Options[6] == 0) App->Options[0] &= ~0x40;
+    if      (Options[7] == 1) App->Options[0] |= 0x80;
+    else if (Options[7] == 0) App->Options[0] &= ~0x80;
+    if      (Options[8] == 1) App->Options[1] |= 0x01;
+    else if (Options[8] == 0) App->Options[1] &= ~0x01;
+    if      (Options[9] == 1) App->Options[1] |= 0x02;
+    else if (Options[9] == 0) App->Options[1] &= ~0x02;
+    if      (Options[10] == 1) App->Options[1] |= 0x04;
+    else if (Options[10] == 0) App->Options[1] &= ~0x04;
+    if      (Options[11] == 1) App->Options[1] |= 0x08;
+    else if (Options[11] == 0) App->Options[1] &= ~0x08;
+    if      (Options[12] == 0) App->Options[1] |= 0x10;
+    else if (Options[12] == 1) App->Options[1] &= ~0x10;
+    if      (Options[13] == 1) App->Options[1] |= 0x20;
+    else if (Options[13] == 0) App->Options[1] &= ~0x20;
+    if      (Options[14] == 1) App->Options[1] |= 0x40;
+    else if (Options[14] == 0) App->Options[1] &= ~0x40;
+    if      (Options[15] == 1) App->Options[1] |= 0x80;
+    else if (Options[15] == 0) App->Options[1] &= ~0x80;
+    if      (Options[16] == 1) App->Options[2] |= 0x01;
+    else if (Options[16] == 0) App->Options[2] &= ~0x01;
+    if      (Options[17] == 1) App->Options[2] |= 0x02;
+    else if (Options[17] == 0) App->Options[2] &= ~0x02;
+    if      (Options[18] == 1) App->Options[2] |= 0x04;
+    else if (Options[18] == 0) App->Options[2] &= ~0x04;
+    if      (Options[19] == 1) App->Options[2] |= 0x08;
+    else if (Options[19] == 0) App->Options[2] &= ~0x08;
+    if      (Options[20] == 1) App->Options[2] |= 0x10;
+    else if (Options[20] == 0) App->Options[2] &= ~0x10;
+    if      (Options[21] == 1) App->Options[2] |= 0x20;
+    else if (Options[21] == 0) App->Options[2] &= ~0x20;
+    if      (Options[22] == 1) App->Options[2] |= 0x40;
+    else if (Options[22] == 0) App->Options[2] &= ~0x40;
+    if      (Options[23] == 1) App->Options[2] |= 0x80;
+    else if (Options[23] == 0) App->Options[2] &= ~0x80;
+    if      (Options[24] == 0) App->Options[3] |= 0x01;
+    else if (Options[24] == 1) App->Options[3] &= ~0x01;
+    if      (Options[25] == 0) App->Options[3] |= 0x02;
+    else if (Options[25] == 1) App->Options[3] &= ~0x02;
+    if      (Options[26] == 1) App->Options[3] |= 0x04;
+    else if (Options[26] == 0) App->Options[3] &= ~0x04;
+    if      (Options[27] == 1) App->Options[3] |= 0x08;
+    else if (Options[27] == 0) App->Options[3] &= ~0x08;
+    if      (Options[28] == 1) App->Options[3] |= 0x10;
+    else if (Options[28] == 0) App->Options[3] &= ~0x10;
+    if      (Options[29] == 1) App->Options[3] |= 0x20;
+    else if (Options[29] == 0) App->Options[3] &= ~0x20;
+    if      (Options[30] == 1) App->Options[3] |= 0x40;
+    else if (Options[30] == 0) App->Options[3] &= ~0x40;
+    if      (Options[31] == 1) App->Options[3] |= 0x80;
+    else if (Options[31] == 0) App->Options[3] &= ~0x80;
+    if      (Options[32] == 1) App->Options[4] |= 0x01;
+    else if (Options[32] == 0) App->Options[4] &= ~0x01;
+    if      (Options[33] == 1) App->Options[4] |= 0x02;
+    else if (Options[33] == 0) App->Options[4] &= ~0x02;
+    if      (Options[34] == 1) App->Options[4] |= 0x04;
+    else if (Options[34] == 0) App->Options[4] &= ~0x04;
+    if      (Options[35] == 1) App->Options[4] |= 0x08;
+    else if (Options[35] == 0) App->Options[4] &= ~0x08;
+    if      (Options[36] == 1) App->Options[4] |= 0x10;
+    else if (Options[36] == 0) App->Options[4] &= ~0x10;
+    if      (Options[37] == 1) App->Options[4] |= 0x20;
+    else if (Options[37] == 0) App->Options[4] &= ~0x20;
+    if      (Options[38] == 1) App->Options[4] |= 0x40;
+    else if (Options[38] == 0) App->Options[4] &= ~0x40;
+    if      (Options[39] == 1) App->Options[4] |= 0x80;
+    else if (Options[39] == 0) App->Options[4] &= ~0x80;
+
+    // FCS = *reinterpret_cast<FlowControlService**>(0x7AE260);
+
+    // if      (NoAutoLoop == 0) FCS->AutoLoop |= 1;
+    // else if (NoAutoLoop == 1) FCS->AutoLoop &= ~1;
+
+    __asm popad
+    __asm jmp Exit
+}
+
 void Init()
 {
     CIniReader iniReader("");
 
-    AspectRatioFix = iniReader.ReadInteger("Main", "AspectRatioFix", 0) == 1;
-    SleepFix = iniReader.ReadInteger("Main", "SleepFix", 0) == 1;
+    AspectRatioFix = iniReader.ReadBoolean("Main", "AspectRatioFix", false);
+    SleepFix = iniReader.ReadBoolean("Main", "SleepFix", false);
     FrameInterval = iniReader.ReadInteger("Main", "FrameInterval", 16);
 
-    Borderless = iniReader.ReadInteger("Options", "Borderless", 0) == 1;
-    NoMusic = iniReader.ReadInteger("Options", "NoMusic", 0) == 1;
-    NoMovies = iniReader.ReadInteger("Options", "NoMovies", 0) == 1;
-    NoMoviesIntro = iniReader.ReadInteger("Options", "NoMoviesIntro", 0) == 1;
+    Fullscreen = iniReader.ReadInteger("Options", "Fullscreen", -1);
+    Borderless = iniReader.ReadBoolean("Options", "Borderless", false);
+    NoMusic = iniReader.ReadBoolean("Options", "NoMusic", false);
+    NoVoices = iniReader.ReadBoolean("Options", "NoVoices", false);
+    NoAutoLoop = iniReader.ReadInteger("Options", "NoAutoLoop", -1);
+
+    Options[0] = iniReader.ReadInteger("Options", "Autorun", -1);
+    Options[1] = iniReader.ReadInteger("Options", "Telnet", -1);
+    Options[2] = iniReader.ReadInteger("Options", "Bundles", -1);
+    Options[3] = iniReader.ReadInteger("Options", "SaveBundles", -1);
+    Options[4] = iniReader.ReadInteger("Options", "Option04", -1);
+    Options[5] = iniReader.ReadInteger("Options", "Option05", -1);
+    Options[6] = iniReader.ReadInteger("Options", "EnemyAI", -1);
+    Options[7] = iniReader.ReadInteger("Options", "ManualCamera", -1);
+    Options[8] = iniReader.ReadInteger("Options", "AlternativeJumping", -1);
+    Options[9] = iniReader.ReadInteger("Options", "CliffStop", -1);
+    Options[10] = iniReader.ReadInteger("Options", "Overlay", -1);
+    Options[11] = iniReader.ReadInteger("Options", "DebugUpdate", -1);
+    Options[12] = iniReader.ReadInteger("Options", "NoHUD", -1);
+    Options[13] = iniReader.ReadInteger("Options", "Screenshots", -1);
+    Options[14] = iniReader.ReadInteger("Options", "Option14", -1);
+    Options[15] = iniReader.ReadInteger("Options", "Option15", -1);
+    Options[16] = iniReader.ReadInteger("Options", "Option16", -1);
+    Options[17] = iniReader.ReadInteger("Options", "Option17", -1);
+    Options[18] = iniReader.ReadInteger("Options", "OnscreenKeyboard", -1);
+    Options[19] = iniReader.ReadInteger("Options", "DebugInfo", -1);
+    Options[20] = iniReader.ReadInteger("Options", "CacheData", -1);
+    Options[21] = iniReader.ReadInteger("Options", "AimingRings", -1);
+    Options[22] = iniReader.ReadInteger("Options", "BuildPS2Sound", -1);
+    Options[23] = iniReader.ReadInteger("Options", "QuitAfterBuild", -1);
+    Options[24] = iniReader.ReadInteger("Options", "NoMoviesIntro", -1);
+    Options[25] = iniReader.ReadInteger("Options", "NoMovies", -1);
+    Options[26] = iniReader.ReadInteger("Options", "Option26", -1);
+    Options[27] = iniReader.ReadInteger("Options", "UnlockAllMissions", -1);
+    Options[28] = iniReader.ReadInteger("Options", "Buttons", -1);
+    Options[29] = iniReader.ReadInteger("Options", "Option29", -1);
+    Options[30] = iniReader.ReadInteger("Options", "Zip", -1);
+    Options[31] = iniReader.ReadInteger("Options", "ShowNetBandwith", -1);
+    Options[32] = iniReader.ReadInteger("Options", "Option32", -1);
+    Options[33] = iniReader.ReadInteger("Options", "Leet", -1);
+    Options[34] = iniReader.ReadInteger("Options", "Xray", -1);
+    Options[35] = iniReader.ReadInteger("Options", "ExportAsciiSchemes", -1);
+    Options[36] = iniReader.ReadInteger("Options", "InvertMouseFP", -1);
+    Options[37] = iniReader.ReadInteger("Options", "InvertMouseTP", -1);
+    Options[38] = iniReader.ReadInteger("Options", "SaveDataFiles", -1);
+    Options[39] = iniReader.ReadInteger("Options", "Option39", -1);
 
     GameSpyHost = iniReader.ReadString("GameSpy", "Host", "gamespy.com");
 
@@ -201,23 +357,13 @@ void Init()
         injector::WriteMemory<BYTE>(0x76FC60, 0, true);
     }
 
-    if (NoMovies || NoMoviesIntro)
+    if (NoVoices)
     {
-        BYTE OptionsByte1 = 0x93;
-        BYTE OptionsByte2 = 0x13;
-        if (NoMoviesIntro)
-        {
-            OptionsByte1 &= ~0x1;
-            OptionsByte2 &= ~0x1;
-        }
-        if (NoMovies)
-        {
-            OptionsByte1 &= ~0x2;
-            OptionsByte2 &= ~0x2;
-        }
-        injector::WriteMemory<BYTE>(0x44A321, OptionsByte1, true);
-        injector::WriteMemory<BYTE>(0x44A323, OptionsByte2, true);
+        injector::MakeJMP(0x4FBCFC, 0x4FBE16);
+        injector::MakeJMP(0x4FDEF9, 0x4FDFC5);
     }
+
+    injector::MakeJMP(0x44D952, OptionsCodeCave);
 
     // GameSpy
 
